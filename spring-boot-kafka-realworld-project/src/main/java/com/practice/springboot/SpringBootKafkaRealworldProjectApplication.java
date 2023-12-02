bd0b37be6f7cd55c630e5a9d4e78591ea4de7181
sayal-aryal <sayalaryal2@gmail.com>
2023-12-02 09:41:12 -0600

    Project initializer

---
+++ spring-boot-kafka-realworld-project/src/main/java/com/practice/springboot/SpringBootKafkaRealworldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaRealworldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaRealworldProjectApplication.class, args);
+	}
+
+}
